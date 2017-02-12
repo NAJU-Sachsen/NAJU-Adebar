097c45dcc38dcf464c5888bfc16aa93cb1828a4a
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2017-02-12 14:34:34 +0100

    Adding utilities for database dumping

---
+++ src/main/java/de/naju/adebar/util/Database.java
@@ -0,0 +1,33 @@
+package de.naju.adebar.util;
+
+import de.naju.adebar.infrastructure.ReadOnlyRepository;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Utility functions for dealing with databases. Mainly for testing purposes. Usage on productive environments may
+ * not be sensible.
+ * @author Rico Bergmann
+ */
+public class Database {
+
+    /**
+     * Plots the content of a database
+     * @param db database to print
+     */
+    public static void dumpDatabase(CrudRepository db) {
+        for (Object entity : db.findAll()) {
+            System.out.println(entity.toString());
+        }
+    }
+
+    /**
+     * Plots the content of a database
+     * @param db database to print
+     */
+    public static void dumpDatabase(ReadOnlyRepository db) {
+        for (Object entity : db.findAll()) {
+            System.out.println(entity.toString());
+        }
+    }
+
+}
