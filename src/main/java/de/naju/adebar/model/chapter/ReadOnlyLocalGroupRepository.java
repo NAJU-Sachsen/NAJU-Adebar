a16f2e69cd16dc46b3c39484d82ee853faa24963
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2017-04-07 15:17:27 +0200

    Providing JavaDoc now

--- src/main/java/de/naju/adebar/model/chapter/ReadOnlyLocalGroupRepository.java
+++ src/main/java/de/naju/adebar/model/chapter/ReadOnlyLocalGroupRepository.java
@@ -7,14 +7,27 @@ import de.naju.adebar.model.newsletter.Newsletter;
 import java.util.Optional;
 
 /**
+ * A repository that provides read-only access to the saved local groups
  * @author Rico Bergmann
  */
 public interface ReadOnlyLocalGroupRepository extends ReadOnlyRepository<LocalGroup, Long> {
 
+    /**
+     * @param name the local group's name to query for
+     * @return an optional containing the local group with that name, otherwise the optional is empty
+     */
     Optional<LocalGroup> findByName(String name);
 
+    /**
+     * @param activist the activist to query for
+     * @return all local groups with the specified person as member
+     */
     Iterable<LocalGroup> findByMembersContains(Activist activist);
 
+    /**
+     * @param newsletter the newsletter to query for
+     * @return an optional containing the local group with that newsletter, otherwise the optional is empty
+     */
     Optional<LocalGroup> findByNewsletter(Newsletter newsletter);
 
 }
