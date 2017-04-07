23b421f24bc9aeeaa2664918f0e5af7c162db893
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2017-04-07 14:13:46 +0200

    Making the stub useable
    ProjectForms do contain information about the local group they
    should be created for. Furthermore getter and setter were added.

--- src/main/java/de/naju/adebar/controller/forms/chapter/ProjectForm.java
+++ src/main/java/de/naju/adebar/controller/forms/chapter/ProjectForm.java
@@ -1,17 +1,24 @@
 package de.naju.adebar.controller.forms.chapter;
 
-import de.naju.adebar.model.human.PersonId;
-
 /**
  * Model POJO for projects. The fields are set by Thymeleaf when the associated form is submitted.
  * @author Rico Bergmann
  */
 public class ProjectForm {
-    public final static String DATE_FORMAT = "DD.MM.yyyy";
+    public final static String DATE_FORMAT = "dd.MM.yyyy";
 
     private String name;
     private String start, end;
-    private PersonId personInCharge;
+    private String personInCharge;
+    private long localGroupId;
+
+    public ProjectForm(String name, String start, String end, String personInCharge, long localGroupId) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.personInCharge = personInCharge;
+        this.localGroupId = localGroupId;
+    }
 
     public ProjectForm() {}
 
@@ -39,11 +46,31 @@ public class ProjectForm {
         this.end = end;
     }
 
-    public PersonId getPersonInCharge() {
+    public String getPersonInCharge() {
         return personInCharge;
     }
 
-    public void setPersonInCharge(PersonId personInCharge) {
+    public void setPersonInCharge(String personInCharge) {
         this.personInCharge = personInCharge;
+    }
+
+    public long getLocalGroupId() {
+        return localGroupId;
+    }
+
+    public void setLocalGroupId(long localGroupId) {
+        this.localGroupId = localGroupId;
+    }
+
+    public boolean hasStart() {
+        return start != null && !start.isEmpty();
+    }
+
+    public boolean hasEnd() {
+        return end != null && !start.isEmpty();
+    }
+
+    public boolean hasPersonInCharge() {
+        return personInCharge != null && !personInCharge.isEmpty();
     }
 }
