279bb8d2d998ef564f7fcb2056687b03f544cd8d
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2017-04-21 17:48:15 +0200

    Adding URL mappings for / and /overview

---
+++ src/main/java/de/naju/adebar/controller/MainController.java
@@ -0,0 +1,19 @@
+package de.naju.adebar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Main controller of the application featuring mappings for the homepage and other off-topic views.
+ * @author Rico Bergmann
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping({"/", "/index", "/overview"})
+    public String showOverview(Model model) {
+        return "redirect:/events";
+    }
+
+}
