070f6a1a99ea37cc4f56417d07515730531615fc
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2017-12-12 17:51:15 +0100

    Polishing

--- src/main/java/de/naju/adebar/infrastructure/LocalDateAttributeConverter.java
+++ src/main/java/de/naju/adebar/infrastructure/LocalDateAttributeConverter.java
@@ -1,14 +1,14 @@
 package de.naju.adebar.infrastructure;
 
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
 import java.sql.Date;
 import java.time.LocalDate;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 /**
  * @author Rico Bergmann
  * @see <a href= "https://www.thoughts-on-java.org/persist-localdate-localdatetime-jpa/">Blog
- *      article</a>
+ *      article about the why and how</a>
  */
 @Converter(autoApply = true)
 public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
