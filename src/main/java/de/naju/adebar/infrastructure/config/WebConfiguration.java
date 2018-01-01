8dabf6fb4cd1810768ea4ee1a0ef62b419d2d074
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2018-01-01 20:20:03 +0100

    Add converters for persons and events

--- src/main/java/de/naju/adebar/infrastructure/config/WebConfiguration.java
+++ src/main/java/de/naju/adebar/infrastructure/config/WebConfiguration.java
@@ -1,14 +1,24 @@
 package de.naju.adebar.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import de.naju.adebar.infrastructure.config.security.WebSecurityConfiguration;
+import de.naju.adebar.infrastructure.formatters.EventConverter;
+import de.naju.adebar.infrastructure.formatters.PersonConverter;
+import de.naju.adebar.model.events.ReadOnlyEventRepository;
+import de.naju.adebar.model.human.ReadOnlyPersonRepository;
 
 /**
  * The general configuration of the web controllers.
  * 
- * We will use this to register our login page.
+ * We will use this to register our login page and multiple formatters:
+ * <ul>
+ * <li>a {@link PersonConverter}</li>
+ * <li>a {@link EventConverter}</li>
+ * </ul>
  * 
  * @author Rico Bergmann
  *
@@ -16,12 +26,31 @@ import de.naju.adebar.infrastructure.config.security.WebSecurityConfiguration;
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
+  private final ReadOnlyPersonRepository personRepo;
+  private final ReadOnlyEventRepository eventRepo;
+
+  public WebConfiguration(ReadOnlyPersonRepository personRepo, ReadOnlyEventRepository eventRepo) {
+    Assert.notNull(personRepo, "Person repository may not be null");
+    Assert.notNull(eventRepo, "Event repository may not be null");
+    this.personRepo = personRepo;
+    this.eventRepo = eventRepo;
+  }
+
   /**
    * Registering new controllers, in this case the login view
    */
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController(WebSecurityConfiguration.LOGIN_ROUTE).setViewName("login");
+  }
+
+  /**
+   * Registering new converters
+   */
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new PersonConverter(personRepo));
+    registry.addConverter(new EventConverter(eventRepo));
   }
 
 }
---
+++ src/main/java/de/naju/adebar/infrastructure/formatters/EventConverter.java
@@ -0,0 +1,30 @@
+package de.naju.adebar.infrastructure.formatters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.Assert;
+import de.naju.adebar.model.events.Event;
+import de.naju.adebar.model.events.EventId;
+import de.naju.adebar.model.events.ReadOnlyEventRepository;
+
+public class EventConverter implements Converter<String, Event> {
+
+  private final ReadOnlyEventRepository eventRepo;
+
+  public EventConverter(ReadOnlyEventRepository eventRepo) {
+    Assert.notNull(eventRepo, "Event repository may not be null");
+    this.eventRepo = eventRepo;
+  }
+
+  @Override
+  public Event convert(String source) {
+    EventId eventId = new EventId(source);
+    Event event = eventRepo.findOne(eventId);
+
+    if (event == null) {
+      throw new IllegalArgumentException("No event with given ID: " + source);
+    }
+
+    return event;
+  }
+
+}
