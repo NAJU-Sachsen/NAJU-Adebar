f2fafb76d743c12436a07cdd4c71882853c532d0
Rico Bergmann <rico.bergmann1@mailbox.tu-dresden.de>
2018-01-04 18:04:33 +0100

    Add release notes column to user accounts table

--- src/main/sql/user_accounts/alter_user_accounts.sql
+++ src/main/sql/user_accounts/alter_user_accounts.sql
@@ -7,4 +7,5 @@ alter table user_account
 add column first_name varchar(255),
 add column last_name varchar(255),
 add column email varchar(255),
+add column read_release_notes bit default false,
 change column person associated_person varchar(255) not null;
