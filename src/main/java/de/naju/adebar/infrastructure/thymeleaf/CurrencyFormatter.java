e5d1f4f9947e162de2e19a7cd520ab876372884a
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-02-02 22:23:06 +0100

    Fix currency formatting

--- src/main/java/de/naju/adebar/infrastructure/thymeleaf/CurrencyFormatter.java
+++ src/main/java/de/naju/adebar/infrastructure/thymeleaf/CurrencyFormatter.java
@@ -3,10 +3,20 @@ package de.naju.adebar.infrastructure.thymeleaf;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
 
+/**
+ * Formatter for instances of {@link MonetaryAmount}
+ *
+ * @author Rico Bergmann
+ */
 public class CurrencyFormatter {
 
+  /**
+   * @param amount the money to format
+   * @return if the money's currency is Euro, then {@code XXX€}, otherwise the {@code toString} will
+   *         be used
+   */
   public String format(MonetaryAmount amount) {
-    if (amount.getCurrency() != Monetary.getCurrency("EUR")) {
+    if (amount.getCurrency().equals(Monetary.getCurrency("EUR"))) {
       return amount.getNumber().toString() + "€";
     }
     return amount.toString();
