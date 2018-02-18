41592a5c93f94b3cf8dd52711ce114e1e6ec2741
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-02-18 01:36:01 +0100

    Polish access denied handler

--- src/main/java/de/naju/adebar/infrastructure/config/security/RedirectingAccessDeniedHandler.java
+++ src/main/java/de/naju/adebar/infrastructure/config/security/RedirectingAccessDeniedHandler.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.InvalidCsrfTokenException;
@@ -19,8 +17,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RedirectingAccessDeniedHandler implements AccessDeniedHandler {
-
-  private static final Logger log = LoggerFactory.getLogger(RedirectingAccessDeniedHandler.class);
 
   /*
    * (non-Javadoc)
