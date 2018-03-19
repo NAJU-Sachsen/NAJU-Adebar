ba5baabb9fdecfb6bdb3c3b877a880fbdbde8203
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-03-19 15:46:28 +0100

    Update the Thymeleaf dialect to Thymeleaf 3.0
    Because this was somehow forgotten in d1732e1acbfb307fea5b6202837d461667e1408f

--- src/main/java/de/naju/adebar/model/persons/qualifications/QualificationRepository.java
+++ src/main/java/de/naju/adebar/model/persons/qualifications/QualificationRepository.java
@@ -1,5 +1,6 @@
 package de.naju.adebar.model.persons.qualifications;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -9,4 +10,15 @@ import org.springframework.data.repository.CrudRepository;
  * @see Qualification
  */
 public interface QualificationRepository extends CrudRepository<Qualification, String> {
+
+  /**
+   * Searches for all qualifications which a referent does not have
+   *
+   * @param referentId the ID of the referent
+   * @return the qualifications
+   */
+  @Query(value = "SELECT q.* FROM qualification q  WHERE q.name NOT IN"
+      + "(SELECT qualifications_name FROM referent JOIN referent_qualifications ON referent.id = referent_qualifications.referent_id WHERE referent.id = ?1)", nativeQuery = true)
+  Iterable<Qualification> findAllQualificationsNotPossessedBy(String referentId);
+
 }
