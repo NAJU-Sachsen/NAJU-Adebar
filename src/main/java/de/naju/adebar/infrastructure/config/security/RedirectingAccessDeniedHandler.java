53f82f7f3bbfbd5af781fd6138e2e9a7d8c2d770
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-03-24 19:07:12 +0100

    Print occuring AccessDeniedException to log

--- src/main/java/de/naju/adebar/infrastructure/config/security/RedirectingAccessDeniedHandler.java
+++ src/main/java/de/naju/adebar/infrastructure/config/security/RedirectingAccessDeniedHandler.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.InvalidCsrfTokenException;
@@ -13,10 +15,12 @@ import org.springframework.stereotype.Component;
  * {@link AccessDeniedHandler} to show the login page if the CSRF-token is invalid.
  *
  * @author Rico Bergmann
- * @see https://stackoverflow.com/a/41633492/5161760
+ * @see <a href="https://stackoverflow.com/a/41633492/5161760">https://stackoverflow.com/a/41633492/5161760</a>
  */
 @Component
 public class RedirectingAccessDeniedHandler implements AccessDeniedHandler {
+
+  private static final Logger log = LoggerFactory.getLogger(RedirectingAccessDeniedHandler.class);
 
   /*
    * (non-Javadoc)
@@ -28,6 +32,9 @@ public class RedirectingAccessDeniedHandler implements AccessDeniedHandler {
   @Override
   public void handle(HttpServletRequest request, HttpServletResponse response,
       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+    log.info("Exception occurred for request to {} with response {} - exception is {}", request,
+        response, accessDeniedException);
 
     if (!response.isCommitted() && accessDeniedException instanceof InvalidCsrfTokenException) {
       request.logout();
