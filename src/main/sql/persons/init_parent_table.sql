c3cda94077c1b0bffd12a542a517e25e6a9735d2
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-03-27 01:32:58 +0200

    Extend parent table SQL to create the table

--- src/main/sql/persons/init_parent_table.sql
+++ src/main/sql/persons/init_parent_table.sql
@@ -3,6 +3,14 @@
  * initializes the parent table with all known parents
  */
 
+create table if not exists parent (
+  id varchar(255) not null,
+  landline_phone varchar(255) default null,
+  work_phone varchar(255) default null,
+  primary key (id),
+  foreign key (id) references person(id)
+);
+
 insert into parent (id, landline_phone, work_phone)
 select distinct parent, null, null
 from parents
