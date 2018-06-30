700b56be5e270640246f435c1a86ba29e93a0a47
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-06-30 22:00:15 +0200

    Do not use person manager in database initializer
    The person manager was deprecated

--- src/main/java/de/naju/adebar/infrastructure/config/DatabaseInitializer.java
+++ src/main/java/de/naju/adebar/infrastructure/config/DatabaseInitializer.java
@@ -1,13 +1,5 @@
 package de.naju.adebar.infrastructure.config;
 
-import com.google.common.collect.Lists;
-import de.naju.adebar.app.security.user.Roles;
-import de.naju.adebar.app.security.user.UserAccountManager;
-import de.naju.adebar.app.security.user.UserAccountRepository;
-import de.naju.adebar.model.core.Email;
-import de.naju.adebar.model.persons.Person;
-import de.naju.adebar.model.persons.PersonFactory;
-import de.naju.adebar.model.persons.PersonManager;
 import java.util.Arrays;
 import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
@@ -17,33 +9,50 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import com.google.common.collect.Lists;
+import de.naju.adebar.app.security.user.Roles;
+import de.naju.adebar.app.security.user.UserAccountManager;
+import de.naju.adebar.app.security.user.UserAccountRepository;
+import de.naju.adebar.model.core.Email;
+import de.naju.adebar.model.persons.Person;
+import de.naju.adebar.model.persons.PersonFactory;
+import de.naju.adebar.model.persons.PersonRepository;
 
+/**
+ * Generates the admin account for the application if necessary.
+ * <p>
+ * The admin's data is specified in the {@code startup.properties} file.
+ *
+ * @author Rico Bergmann
+ */
 @PropertySource("classpath:startup.properties")
 @Component
 public class DatabaseInitializer {
 
   private static final Logger log = LoggerFactory.getLogger(DatabaseInitializer.class);
 
-  private Environment environment;
-  private PersonManager personManager;
-  private PersonFactory personFactory;
-  private UserAccountManager userAccountManager;
-  private UserAccountRepository userAccountRepo;
+  private final Environment environment;
+  private final PersonRepository personRepo;
+  private final PersonFactory personFactory;
+  private final UserAccountManager userAccountManager;
+  private final UserAccountRepository userAccountRepo;
 
   @Autowired
-  public DatabaseInitializer(Environment environment, PersonManager personManager,
+  public DatabaseInitializer(Environment environment, PersonRepository personRepo,
       PersonFactory personFactory, UserAccountManager userAccountManager,
       UserAccountRepository userAccountRepo) {
-    Object[] params =
-        {environment, personManager, personFactory, userAccountManager, userAccountRepo};
+    Object[] params = {environment, personRepo, personFactory, userAccountManager, userAccountRepo};
     Assert.noNullElements(params, "No parameter may be null: " + Arrays.toString(params));
     this.environment = environment;
-    this.personManager = personManager;
+    this.personRepo = personRepo;
     this.personFactory = personFactory;
     this.userAccountManager = userAccountManager;
     this.userAccountRepo = userAccountRepo;
   }
 
+  /**
+   * Inflates the database with the necessary data.
+   */
   @PostConstruct
   public void setup() {
     if (!isEmptyDatabase()) {
@@ -53,15 +62,21 @@ public class DatabaseInitializer {
     Person admin = loadPersonFromProperties();
     String username = loadUsernameFromProperties();
     String password = loadInitialPasswordFromProperties();
-    personManager.savePerson(admin);
+    personRepo.save(admin);
     userAccountManager.createFor(username, password, admin, Lists.newArrayList(Roles.ROLE_ADMIN),
         false);
   }
 
+  /**
+   * Checks if the database contains an user with admin privileges.
+   */
   private boolean isEmptyDatabase() {
     return userAccountRepo.findByAuthoritiesContains(Roles.ROLE_ADMIN).isEmpty();
   }
 
+  /**
+   * Generates the admin person according to the properties stated in {@code startup.properties}
+   */
   private Person loadPersonFromProperties() {
     String firstName = environment.getProperty("adebar.admin.first-name");
     String lastName = environment.getProperty("adebar.admin.last-name");
@@ -69,10 +84,16 @@ public class DatabaseInitializer {
     return personFactory.buildNew(firstName, lastName, Email.of(email)).create();
   }
 
+  /**
+   * Fetches the username of the admin from the properties.
+   */
   private String loadUsernameFromProperties() {
     return environment.getProperty("adebar.admin.username");
   }
 
+  /**
+   * Fetches the admin's password from the properties.
+   */
   private String loadInitialPasswordFromProperties() {
     return environment.getProperty("adebar.admin.password");
   }
