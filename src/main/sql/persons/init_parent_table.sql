10d1327197d1301e7c18d6f41b4f612210eda0fd
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2018-10-08 13:56:33 +0200

    Fix wrong character set in init_parent_table.sql

--- src/main/sql/persons/init_parent_table.sql
+++ src/main/sql/persons/init_parent_table.sql
@@ -9,7 +9,10 @@ create table if not exists parent (
   work_phone varchar(255) default null,
   primary key (id),
   foreign key (id) references person(id)
-);
+) character set utf8;
+
+alter table person
+add column parent bit(1) default false;
 
 insert into parent (id, landline_phone, work_phone)
 select distinct parent, null, null
@@ -19,7 +22,3 @@ where parent not in (select id from parent);
 update person
 set parent=true
 where id in (select parent from parents);
-
-update person
-set parent=false
-where id not in (select parent from parents);
